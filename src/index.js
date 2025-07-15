b76c2e26300c242473a0e389b288ede86fd576fd
MedeAnaCode <114002878+MedeAnaCode@users.noreply.github.com>
2025-07-16 01:08:04 +0300

    Restructure comments and cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
 
-
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
@@ -10,16 +9,3 @@ root.render(
         <App/>
     </React.StrictMode>
 );
-
-// import RecipeCard from './components/Recipes-card.jsx';
-// import saladImage from './img/salad.png';
-// import data from './api.js';
-// root.render(
-//     <React.StrictMode>
-//         <RecipeCard
-//             imageUrl={saladImage}
-//             title="Гречка"
-//             preparingText="Описание процесса приготовления гречки."
-//         />
-//     </React.StrictMode>
-// );
