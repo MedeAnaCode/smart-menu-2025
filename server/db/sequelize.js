eb15238d5cfdaeeebcf6c807cc4381c6fc560828
MedeAnaCode <114002878+MedeAnaCode@users.noreply.github.com>
2025-07-16 00:31:00 +0300

    Adjust Docker setup and remove legacy scripts

--- server/db/sequelize.js
+++ server/db/sequelize.js
@@ -1,10 +1,15 @@
 const {Sequelize}= require("sequelize");
 
-const sequelize = new Sequelize("postgres", "Nastya", "run", {
-    dialect: "postgres",
-    host: "127.0.0.1",
-    port: "5432",
-    schema: "public",
-});
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DB || "postgres",
+    process.env.POSTGRES_USER || "Nastya",
+    process.env.POSTGRES_PASSWORD || "run",
+    {
+        dialect: "postgres",
+        host: process.env.POSTGRES_HOST || "127.0.0.1",
+        port: process.env.POSTGRES_PORT || "5432",
+        schema: "public",
+    }
+);
 
 module.exports = sequelize;
