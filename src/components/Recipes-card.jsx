20a9090caa4518e9de3baf19beebfbc3e71e88a1
MedeAnaCode <medentsova.anastasia@yandex.ru>
2025-11-11 18:04:15 +0300

    ÐStylize buttons in recipe-cards

--- src/components/Recipes-card.jsx
+++ src/components/Recipes-card.jsx
@@ -22,21 +22,28 @@ function RecipeCard ({ title, preparingText, imageUrl, ingredients, id, servings
                 <span className="recipe-card__option-title">Приготовление:</span>
                 <p className="recipe-card__preparing">{preparingText}</p>
             </div>
-
+        <div className="recipe-card__button-block">
             <button
                 type="button"
-                className="button-dark"
+                className="recipe-card__button button-dark"
                 onClick={handleDelete}
             >
-                Удалить
+                <span className="visually-hidden">
+                    Удалить.
+                </span>
+                <img className="recipe-card__button-icon" width="14" height="14" src='/img/icons/delete.svg'/>
             </button>
             <button
                 type="button"
-                className="button-dark"
+                className="recipe-card__button button-dark"
                 onClick={handleEdit}
             >
-                Редактировать
+                <span className="visually-hidden">
+                    Редактировать.
+                </span>
+                <img className="recipe-card__button-icon" width="14" height="14" src='/img/icons/edit.svg'/>
             </button>
+        </div>
         </article>
     );
 }
